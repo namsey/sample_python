8d2f845fd18d7ec67b693e11ba338cf4ce165175
NAMAN SINGH <namansingh48@gmail.com>
2023-06-07 11:02:14 +0530

    Update mergeArray_python.py

--- mergeArray_python.py
+++ mergeArray_python.py
@@ -1,44 +1,87 @@
-import asyncio
-import json
-import aiohttp
-
-
-async def main():
-    api_key = "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"
-    input_file = r"C:\Users\176381\openai\input.txt"  # Replace with the path to your TXT file
-    output_file = input_file.replace(".txt", "_output.txt")  # Create output file path by replacing file extension
-    prmpt = input("Enter user prompt: ")
-    with open(input_file) as f:
-        lines = f.readlines()
-        formatted_str = ''.join(lines).replace('\r\n', '\\n').replace('\r', '').replace('\n', '\\\\n ')
-        input_str = prmpt + r"\n" + r"\n" + formatted_str
-        await process_input(input_str, api_key, output_file)
-
-
-async def process_input(input_str, api_key, output_file):
-    async with aiohttp.ClientSession() as session:
-        headers = {
-            "Authorization": f"Bearer {api_key}",
-            "Content-Type": "application/json",
-        }
-        data = {
-            "prompt": input_str,
-            "max_tokens": 1000,
-            "temperature": 0.7,
-        }
-        async with session.post(
-                "https://api.openai.com/v1/engines/text-davinci-003/completions",
-                headers=headers,
-                data=json.dumps(data),
-        ) as response:
-            json_data = await response.json()
-            print(json_data)
-            output_text = json_data["choices"][0]["text"] + "\n"
-            print(output_text)
-            output_text = output_text.replace('\\n', '\n')
-            with open(output_file, 'w') as f:  # Save output to file
-                f.write(output_text)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class Check_Box implements ActionListener
+{
+	static JTextField text;
+	static Checkbox checkbox;
+	//Driver function
+	public static void main(String args[])
+	{
+		//Create a frame
+		JFrame frame=new JFrame("Check Box");
+		frame.setSize(500,500);
+		frame.setBackground(Color.white);
+		frame.setLayout(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Create a textfield
+		text=new JTextField();
+		text.setBounds(0,0,500,50);
+		frame.add(text);
+		//Create 3 buttons
+		JButton get_st=new JButton("Get State");
+		JButton set_st=new JButton("Set State");
+		JButton get_lb=new JButton("Get Label");
+		get_st.setBounds(50,80,100,50);
+		set_st.setBounds(180,80,100,50);
+		get_lb.setBounds(310,80,100,50);
+		frame.add(get_st);
+		frame.add(set_st);
+		frame.add(get_lb);
+		//Create an object of class
+		Check_Box obj=new Check_Box();
+		//Associate ActionListener with the buttons
+		get_st.addActionListener(obj);
+		set_st.addActionListener(obj);
+		get_lb.addActionListener(obj);
+		//Create a checkbox
+		checkbox=new Checkbox("Sample Checkbox");
+		checkbox.setBounds(150,200,200,80);
+		frame.add(checkbox);
+		//Display the frame
+		frame.setVisible(true);
+	}
+	//function to call different methods based on user's choice of button
+        @Override
+	public void actionPerformed(ActionEvent e)
+	{
+		String option=e.getActionCommand();
+		if(option.equals("Get State"))
+			get_State();
+		else if(option.equals("Set State"))
+			set_State();
+		else
+			get_Label();
+	}
+	//function to get the current state of the checkbox
+	public void get_State()
+	{
+		boolean state=checkbox.getState();
+		if(state==true)
+			text.setText("State of Checkbox : Checked");
+		else
+			text.setText("State of Checkbox : Un-Checked");
+	}
+	//function to change the current state of the checkbox
+	public void set_State()
+	{
+                text.setText("State of Checkbox changed from ");
+		boolean state=checkbox.getState();
+		if(state==true)
+		{
+			checkbox.setState(false);
+			text.setText(text.getText()+"Checked to Un-Checked");
+		}
+		else
+		{
+			checkbox.setState(true);
+			text.setText(text.getText()+"Un-Checked to Checked");
+		}
+	}
+	//function to get the label of the checkbox
+	public void get_Label()
+	{
+		text.setText("Label of the checkbox is : ");
+                text.setText(text.getText()+checkbox.getLabel());	
+	}
+}
